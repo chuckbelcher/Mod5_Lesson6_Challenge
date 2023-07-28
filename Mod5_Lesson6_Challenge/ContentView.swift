8d7e29cb85df3b09d5d80783b1d04f87b83e0118
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-28 18:44:29 -0400

    Initial Commit

---
+++ Mod5_Lesson6_Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mod5_Lesson6_Challenge
+//
+//  Created by Chuck Belcher on 7/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mod5_Lesson6_Challenge/Mod5_Lesson6_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod5_Lesson6_ChallengeApp.swift
+//  Mod5_Lesson6_Challenge
+//
+//  Created by Chuck Belcher on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Mod5_Lesson6_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
