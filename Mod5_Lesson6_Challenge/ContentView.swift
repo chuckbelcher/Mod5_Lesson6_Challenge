f1b04cc75b0af8dc73e7a49711687d9c8d9fbeca
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-28 21:14:12 -0400

    Description added, Challenged completed.

--- Mod5_Lesson6_Challenge/ContentView.swift
+++ Mod5_Lesson6_Challenge/ContentView.swift
@@ -24,15 +24,27 @@ struct ContentView: View {
     
     
     var body: some View {
-        VStack {
+        VStack  {
             if url != nil {
                 VideoPlayer(player: AVPlayer(url: url!))
                     .frame(height: 250)
             }
             
-            //TODO: Add Description
-            Text("Going to add Description here")
+            
+                
+                VStack (alignment: .leading) {
+                    //TODO: Add Description
+                    Text("Description")
+                        .font(.title)
+                        
+                    ScrollView {
+                    Text("Bacon ipsum dolor amet jerky flank kevin quis sint meatloaf id pork loin tongue corned beef in excepteur capicola chuck. Nisi ball tip occaecat aliquip. Nisi culpa id irure kielbasa laborum. Occaecat mollit corned beef adipisicing velit pork chop jerky alcatra do. Cupim pancetta t-bone occaecat. Officia sausage deserunt, ea enim non flank pariatur turkey cupidatat turducken in. Et beef ribs bacon meatloaf ad ullamco. Pork chop excepteur esse ut. Ut pork chop consequat, laboris pork loin picanha tongue pancetta elit voluptate non doner prosciutto. Deserunt meatloaf ground round tri-tip landjaeger turkey eiusmod leberkas bresaola brisket laboris. Porchetta pariatur boudin labore mollit ribeye consectetur aliqua consequat id non. Sed boudin excepteur, aliquip pork belly quis in capicola lorem esse doner consequat strip steak tongue dolore. Reprehenderit brisket consectetur, cillum ipsum ball tip sed officia bresaola pork frankfurter turducken. Capicola voluptate minim in, commodo ham officia picanha sirloin tempor short ribs leberkas ad nisi. Spare ribs short loin ham hock pork chop elit rump adipisicing, consequat short ribs pancetta alcatra t-bone.")
+                }
+                
+            }
+                .padding(.horizontal)
         }
+        
     }
 }
 
