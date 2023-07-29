4f700d54e295362c81328c6374180f17fc8e4289
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-29 13:00:53 -0400

    Solution identified thanks to Chris. Added  the target Membership to the video file.

--- Mod5_Lesson6_Challenge/ContentView.swift
+++ Mod5_Lesson6_Challenge/ContentView.swift
@@ -11,13 +11,13 @@ import AVKit
 struct ContentView: View {
 
     //This does not load
-    //let url = URL(fileURLWithPath: Bundle.main.path(forResource: "sample", ofType: "mp4")!)
+    let url = URL(fileURLWithPath: Bundle.main.path(forResource: "sample", ofType: "mp4")!)
     
     //This does not load
     //let url = URL(string: "https:/video.nest.com/clip/32c1271db1d74a73906355b149085db7.mp4")
     
     //this one works
-    let url = URL(string: "https://codewithchris.github.io/learningJSON/Learn%20Swift%20for%20Beginners%20Lesson%201%20-%20Variables%20(Swift%205%20compatible)-2OZ07fklur8.mp4")
+    //let url = URL(string: "https://codewithchris.github.io/learningJSON/Learn%20Swift%20for%20Beginners%20Lesson%201%20-%20Variables%20(Swift%205%20compatible)-2OZ07fklur8.mp4")
     
     //This does not load
     //let url = URL(string: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4")
@@ -25,11 +25,17 @@ struct ContentView: View {
     
     var body: some View {
         VStack  {
-            if url != nil {
-                VideoPlayer(player: AVPlayer(url: url!))
-                    .frame(height: 250)
-            }
             
+            //Use with local file url
+                VideoPlayer(player: AVPlayer(url: url))
+                    .frame(height: 250)
+
+            
+            //Use with string based URL
+            //if url != nil {
+            //    VideoPlayer(player: AVPlayer(url: url))
+            //        .frame(height: 250)
+            //}
             
                 
                 VStack (alignment: .leading) {
